08b353c7c95bc011e547e9d2bb664291fd9254e1
Chi Tran <chidtran@gmail.com>
2019-01-29 15:58:11 -0700

    Add Board class and test skeleton

---
+++ lib/board.rb
@@ -0,0 +1,14 @@
+class Board
+    attr_reader :cells
+
+    def initialize
+        @cells = cells 
+    end
+  
+    def valid_coordinate?(coordinate)
+    end
+  
+    def valid_placement?(name, coordinates)
+    end
+end
+  
