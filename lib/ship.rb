68a043fa5584be8fdd9cae2d1ea593ca46b935ad
Martha Troubh <indexer@icloud.com>
2019-01-29 14:51:22 -0700

    Complete Ship class skeleton

--- lib/ship.rb
+++ lib/ship.rb
@@ -6,4 +6,21 @@ class Ship
     @name = name
     @length = length
   end
+
+  def health
+    #related to length until hit
+  end
+
+  def sunk?
+    if health = 0
+      true
+    else
+      false
+    end
+  end
+
+  def hit
+    # when hit +1 health -1
+  end
+
 end
