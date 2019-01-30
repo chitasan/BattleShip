5a619775a21830db32204b6c6c0a310ce888e76c
Martha Troubh <indexer@icloud.com>
2019-01-30 16:21:53 -0700

    Add more tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -30,13 +30,41 @@ require './lib/cell'
     cruiser = Ship.new("Cruiser", 3)
     assert_equal cruiser, cell.place_ship(cruiser)
     assert_equal cruiser, cell.ship
-#   end
+  end
 
-  def if_ship_fired_upon
+  def test_if_ship_was_not_fired_upon?
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
-    asser
-
+    refute false, cell.fired_upon?
   end
+
+  def test_the_ships_health_after_hit
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 2, ship.health
+  end
+
+  def test_the_ship_was_fired_upon
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert true, cell.fired_upon?
+  end
+
+  def test_that_cell_1_exists
+    cell_1 = Cell.new("B4")
+    assert_instance_of Cell, cell_1
+  end
+
+  def test_it_represents_a_cell_that_has_not_been_fired_upon
+    cell_1 = Cell.new("B4")
+    refute false, cell_1.fire_upon
+    assert_equal ".", cell_1.render
+  end
+
+  def test_it_represents_a_miss
+    cell = Cell.new("B4")
+    assert_equal "M", cell_1.render
+  end     
+
 
 end
