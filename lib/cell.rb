0f6fe03c0b30ba1361b4527bde5820a7fb6d541e
Chi Tran <chidtran@gmail.com>
2019-01-30 23:39:36 -0700

    Add fired_upon and render methods to Cell class, edit tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,13 +1,14 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false 
   end
 
   def empty?
-    @ship == nil
+    @ship = nil
   end
 
   def place_ship(ship)
@@ -18,7 +19,23 @@ class Cell
     @fired_upon
   end
 
-  def fired_upon #(fire_upon)
-    
+  def fired_upon 
+    @fired_upon = true 
+    if @ship != nil 
+      @ship.hit
+    end
   end
+
+  def render(boolean = nil) #nil for optional boolean arg (see iteration 1 directions)
+    if @ship != nil && @fired_upon = true #refactor? 
+      "H"
+    elsif @ship = nil && @fired_upon = true
+      "M"
+    elsif @ship != nil && @fired_upon = true 
+      "X"
+    else 
+      "."
+    end 
+  end 
+
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -13,58 +13,49 @@ require './lib/cell'
     cell = Cell.new("B4")
     assert_equal "B4", cell.coordinate
     assert_nil nil, cell.ship #moved this from a separate test as ship is an attribute, too
+    refute false, cell.fired_upon #added this as an initialized method as it'll be called on in other methods 
   end
 
-  def test_if_cell_has_no_ships #renamed test 
+  def test_if_cell_has_no_ships #renamed test. should we go with empty?
     cell = Cell.new("B4")
     assert true, cell.empty?
   end
 
-  def test_if_ship_can_be_placed_in_cell
+  def test_if_ship_can_be_added_into_cell #how descriptive do i need to be with mult assertions? 
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
-    assert_equal cruiser, cell.place_ship(cruiser)
+    cell.place_ship(cruiser)
     assert_equal cruiser, cell.ship
+    refute false, cell.empty?
   end
 
-  def test_if_cell_is_fired_upon?
-    cell = Cell.new("B4") #removed instance of ship as we are testing am empty cell 
+  def test_if_cell_is_fired_upon_and_test_ships_health_after_hit 
+    cell = Cell.new("B4") #removed instance of ship as we are testing an empty cell 
     refute false, cell.fired_upon?
 
-    cell = Cell.new("B4") #added this test to check if we can assert true 
+    cell = Cell.new("B4") #added this test to check if we can assert true with ship in cell 
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser) 
-    cell.fire_upon
-    assert true, cell.fired_upon? 
-
+    assert true, cell.fired_upon? #do i need to do both assertions? 
+    assert true, cell.fired_upon
+    assert_equal 2, cell.ship.health #moved assertions to test health, should i make it separate test method? 
   end
 
-  def test_the_ships_health_after_hit #rename to fire_upon instead of hit 
-    skip
-    cell = Cell.new("B4")
-    cruiser = Ship.new("Cruiser", 3)
-    cell.place_ship(cruiser) #need to place ship in cell, listing instances does not make it placed 
-    cell.fire_upon
-    assert_equal 2, cell.ship.health #add in cell as the ship is in this cell 
-  end
+  #def test_that_cell_1_exists 
+    #cell_1 = Cell.new("B4")
+    #assert_instance_of Cell, cell_1
+  #end
+  #we have already tested that we can assert an instance of Cell, so we don't need to do it again as our code works; I believe we are to proceed with the assumption that these are real instances. This is a good question to ask someone, maybe? We are to use cell_1 and cell_2 to test when/if cell is fired upon, results to print on board as miss (M), hit (H), sunk (X), or not fired upon (.) on specific cell coordinate. 
 
-  def test_the_ship_was_fired_upon
-    skip
-    cell = Cell.new("B4") #removed instance of ship as it is not needed since it's not placed in cell 
-    assert true, cell.fired_upon?
-  end
-
-  def test_that_cell_1_exists
-    skip
+  def test_results_to_print_the_board_if_cell_is_fired_upon
     cell_1 = Cell.new("B4")
-    assert_instance_of Cell, cell_1
-  end
+    assert_equal ".", cell_1.render 
 
-  def test_it_represents_a_cell_that_has_not_been_fired_upon
-    skip
     cell_1 = Cell.new("B4")
-    refute false, cell_1.fire_upon
-    assert_equal ".", cell_1.render
+    cell_1.fired_upon
+    assert_equal "M", cell_1.render
+
+    cell_2 = Cell.new("C3")
   end
 
   def test_it_represents_a_miss
