cc9636cbe3e7f579ffdd21e6f77865dc6453950e
Chi Tran <chidtran@gmail.com>
2019-01-31 13:35:57 -0700

    Refactor and fix Cell test

--- lib/cell.rb
+++ lib/cell.rb
@@ -19,28 +19,34 @@ class Cell
     @fired_upon
   end
 
-<<<<<<< HEAD
-  def fired_upon #(fire_upon)
-    
-=======
   def fired_upon 
     @fired_upon = true 
     if @ship != nil 
       @ship.hit
     end
->>>>>>> 0f6fe03c0b30ba1361b4527bde5820a7fb6d541e
+
+  def render(boolean = false) #refactor, case statement?
+    if @ship != nil && @fired_upon == true 
+     "H"
+      elsif @ship == nil && @fired_upon == true
+        "M"
+      elsif @fired_upon == true && @ship.sunk? == true && @ship == nil
+        "X"
+      elsif @ship.sunk? != true && boolean == true && @fired_upon == false
+        "S"
+      else 
+        "."
+      end 
+    end 
   end
 
-  def render(boolean = nil) #nil for optional boolean arg (see iteration 1 directions)
-    if @ship != nil && @fired_upon = true #refactor? 
-      "H"
-    elsif @ship = nil && @fired_upon = true
-      "M"
-    elsif @ship != nil && @fired_upon = true 
-      "X"
-    else 
-      "."
-    end 
-  end 
-
 end
+
+=begin
+M
+.
+X
+H 
+S 
+.
+=end 
