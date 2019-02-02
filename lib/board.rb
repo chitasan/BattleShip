e7d1307e12590148b8cb1ffb0778eba90dfb4fef
Chi Tran <chidtran@gmail.com>
2019-02-02 01:22:11 -0700

    Refactor cells method

--- lib/board.rb
+++ lib/board.rb
@@ -1,51 +1,48 @@
 require './lib/cell'
 
 class Board
-    attr_reader :cells, :column, :row
+    attr_reader :cells, :columns, :rows
 
     def initialize
-
-        @cells =       #refactor?
-            {
-                "A1" => Cell.new("A1"),
-                "A2" => Cell.new("A2"),
-                "A3" => Cell.new("A3"),
-                "A4" => Cell.new("A4"),
-                "B1" => Cell.new("B1"),
-                "B2" => Cell.new("B2"),
-                "B3" => Cell.new("B3"),
-                "B4" => Cell.new("B4"),
-                "C1" => Cell.new("C1"),
-                "C2" => Cell.new("C2"),
-                "C3" => Cell.new("C3"),
-                "C4" => Cell.new("C4"),
-                "D1" => Cell.new("D1"),
-                "D2" => Cell.new("D2"),
-                "D3" => Cell.new("D3"),
-                "D4" => Cell.new("D4"),
-            }
+        @columns = ("A".."D")
+        @rows = (1..4)
+        @cells = {} 
     end
 
-    def valid_coordinate?(coordinate) #check to see if value exists in hash 
-        @cells.key?(coordinate)
-
-
+    def create_cells
+        columns.each do |column|
+            rows.each do |row|
+            key = "#{column}#{row}"
+            cells[key] = Cell.new(key)
+            end
+        end
+        cells
     end
 
-    def same_length?(name, coordinates)
-        @coordinates.length == @ship.length 
+    def valid_coordinate?(coordinate) 
+        create_cells.key?(coordinate)
+    end
+
+    def same_length?(ship, coordinates)
+        ship.length == coordinates.length
     end 
 
-    def consecutive?
+    #def consecutive?
         #coordinates.each_cons
-    end
+    #end
 
-    def diagonal? #coordinates cannot be diagonal 
-    end 
+    #def diagonal? #coordinates cannot be diagonal 
+    #end 
 
-    #name = Ship object, and array of Coords 
-    def valid_placement?(name, coordinates) 
+    #name = Ship object, and array of Coords  
+    #def valid_placement?(name, coordinates) 
         #same_length? && consecutive? && diagonal tests must pass before we can do this one. 
-    end
+    #end
 
 end 
+
+board = Board.new
+cruiser = Ship.new("Cruiser", 3)
+p board.create_cells
+
+
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,7 +1,5 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-#require './lib/ship' --Cell class already requires ship in it's file, so I don't think we need this for it to run properly
-#require './lib/cell' --Board class file requires cell 
 require './lib/board'
 
 
@@ -9,14 +7,29 @@ class BoardTest < Minitest::Test
 
   def test_board_exists
     board = Board.new
+
     assert_instance_of Board, board
   end
 
-  def test_it_has_16_cells
+  def test_that_cells_attribute_and_is_a_hash
     board = Board.new
 
-    assert_equal 16, board.cells.length
+    assert_equal Hash, board.cells.class
+    #assert_equal 16, board.cells.length -- cannot test till as actual = 0
+  end
+
+  def test_it_has_16_cells_with_key_value_pair
+    board = Board.new
+
+    assert_equal 16, board.create_cells.keys.length
+    assert_equal 16, board.create_cells.values.length 
   end 
+ 
+  # def test_key_value_points_to_cell_objects #do i test for this? how? 
+  #   board = Board.new
+
+  #   assert_equal expected, actual 
+  # end 
 
   def test_coordinates_are_valid
     board = Board.new
@@ -25,7 +38,7 @@ class BoardTest < Minitest::Test
     assert board.valid_coordinate?("D4")
   end
 
-  def test_coordinates_valid 
+  def test_coordinates_not_valid 
     board = Board.new
   
     refute board.valid_coordinate?("A5")
@@ -34,12 +47,9 @@ class BoardTest < Minitest::Test
   end
 
   def test_if_coordinates_and_ship_are_same_length
-    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
-
-    
 
     refute board.same_length?(cruiser, ["A1", "A2"])
     assert board.same_length?(cruiser, ["A1", "A2", "A3"])
@@ -48,14 +58,19 @@ class BoardTest < Minitest::Test
   end 
 
   def test_if_coordinates_are_consecutive
-
+    skip
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
 
     refute board.valid_placement?(cruiser, ["A1", "A2", "A4"])
     refute board.valid_placement?(submarine, ["A1", "C1"])
     refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    refute board.valid_placement?(cruiser, ["C1", "B1"])
   end
 
   def test_if_coordinates_are_diagonal
+    skip
   end 
 
   def test_if_placements_are_valid 
@@ -68,7 +83,8 @@ class BoardTest < Minitest::Test
     refute board.valid_placement?(submarine, ["A2", "A3", "A4"])
   end
  
-  def test_placements_are_not_valid #this test is the refute assertion in the test above 
+  def test_placements_are_not_valid
+    skip #this test is the refute assertion in the test above 
   end
 
 end
