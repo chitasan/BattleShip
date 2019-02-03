14cd108bae2af775fdb1d6c6cd67b662164ce925
Chi Tran <44073660+chitasan@users.noreply.github.com>
2019-02-02 18:19:40 -0700

    Update cell_test.rb

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -2,7 +2,6 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/cell'
 
-
  class CellTest < Minitest::Test
   def test_cell_exists
     cell = Cell.new("B4")
@@ -12,7 +11,6 @@ require './lib/cell'
   def test_it_has_attributes
     cell = Cell.new("B4")
     assert_equal "B4", cell.coordinate
-    assert_nil nil, cell.ship #moved this from a separate test as ship is an attribute, too
   end
  
   def test_cell_has_no_ships_by_default
@@ -51,9 +49,10 @@ require './lib/cell'
     assert true, cell.fired_upon? #query
   end
 
-  def test_ship_health_after_cell_after_fire_upon
+  def test_ship_health_after_cell_is_fire_upon
     cell = Cell.new("B4") 
     cruiser = Ship.new("Cruiser", 3)
+   
     cell.place_ship(cruiser) 
     cell.fire_upon
 
@@ -75,7 +74,6 @@ require './lib/cell'
   end
 
   def test_board_results_when_cell_has_not_been_fired_upon_and_contains_a_ship
-    
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
 
@@ -87,8 +85,8 @@ require './lib/cell'
   def test_board_result_when_cell_has_been_fired_upon_and_contains_ship
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
+   
     cell_2.place_ship(cruiser)
-    
     cell_2.fire_upon
 
     assert_equal "H", cell_2.render
@@ -107,61 +105,7 @@ require './lib/cell'
     cruiser.hit
     cruiser.hit
 
-<<<<<<< HEAD
     assert cruiser.sunk?
     assert_equal "X", cell_2.render
-<<<<<<< Updated upstream
-  end
-=======
   end     
-=======
-    assert_equal cruiser, cell.place_ship(cruiser)
-    assert_equal cruiser, cell.ship
-  end
->>>>>>> 4c703417d49a7e53e7f6c4a307f72c5553b8fa43
-
-  def test_if_cell_is_fired_upon?
-    cell = Cell.new("B4") #removed instance of ship as we are testing am empty cell 
-    refute false, cell.fired_upon?
-
-    cell = Cell.new("B4") #added this test to check if we can assert true 
-    cruiser = Ship.new("Cruiser", 3)
-    cell.place_ship(cruiser) 
-    cell.fire_upon
-    assert true, cell.fired_upon? 
-<<<<<<< HEAD
-  end
-
-  def test_the_ships_health_after_hit #rename to fire_upon instead of hit 
-    skip
-    cell = Cell.new("B4")
-    cruiser = Ship.new("Cruiser", 3)
-    cell.place_ship(cruiser) #need to place ship in cell, listing instances does not make it placed 
-    cell.fire_upon
-    assert_equal 2, cell.ship.health #add in cell as the ship is in this cell 
-  end
-
-=======
-  end
-
-  def test_the_ships_health_after_hit #rename to fire_upon instead of hit 
-    skip
-    cell = Cell.new("B4")
-    cruiser = Ship.new("Cruiser", 3)
-    cell.place_ship(cruiser) #need to place ship in cell, listing instances does not make it placed 
-    cell.fire_upon
-    assert_equal 2, cell.ship.health #add in cell as the ship is in this cell 
-  end
-
->>>>>>> 4c703417d49a7e53e7f6c4a307f72c5553b8fa43
-  def test_the_ship_was_fired_upon
-    skip
-    cell = Cell.new("B4") #removed instance of ship as it is not needed since it's not placed in cell 
-    assert true, cell.fired_upon?
-  end
-
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> 4c703417d49a7e53e7f6c4a307f72c5553b8fa43
-end
+ end
