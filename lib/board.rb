7f43926f7e98e656af845a0b0cf2cd5a1be89423
Chi Tran <chidtran@gmail.com>
2019-02-03 20:22:13 -0700

    Add methods and tests to Board up to valid placement

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,11 @@
 require './lib/cell'
 
 class Board
-    attr_reader :cells, :y_coordinate, :x_coordinate
+    attr_reader :cells, :x_coordinate, :y_coordinate
 
     def initialize
-        @y_coordinate = ("A".."D")
         @x_coordinate = (1..4)
+        @y_coordinate = ("A".."D")
         @cells = {}
     end
 
@@ -23,24 +23,57 @@ class Board
         create_cells.key?(coordinate)
     end
 
-    def ordinals_array
-        y_coordinate.map do |letter|
-            letter.ord
+    def same_length?(ship, coordinates)
+        ship.length == coordinates.length
+    end 
+
+    def split_coordinates(ship, coordinates)
+        split_coordinates = []
+        coordinates.map do |coordinate|
+            split_coordinates << coordinate.split('').to_a
+        end 
+        split_coordinates.flatten
+    end 
+
+    def y_coordinates_ordinal_values(ship, coordinates)
+        split_coords = split_coordinates(ship, coordinates)
+
+        letters = split_coords.reject do |coordinate| 
+            ["1", "2", "3", "4"].include?(coordinate)
+        end 
+
+        ordinal_value = letters.map do |letter|
+            letter.to_s.ord
         end 
     end
 
-    def numbers_array
-        x_coordinate.to_a
+    def x_coordinates_numbers(ship, coordinates)
+        split_coords = split_coordinates(ship, coordinates)
+
+        numbers = split_coords.reject do |coordinate| 
+            ["A", "B", "C", "D"].include?(coordinate)
+        end 
+
+        numbers.map do |number|
+            number.to_i
+        end 
     end 
 
-    def y_coordinates_to_ordinals
-        coordinate_ordinal_hash = Hash.new
-        @y_coordinate.each_with_index { |letter|
-            coordinate_ordinal_hash[letter] = letter.ord }
-        coordinate_ordinal_hash  
+    def letters_consecutive?(ship, coordinates)
+        ordinals = y_coordinates_ordinal_values(ship, coordinates)
+        ordinals.each_cons(2).all? do |x,y|
+            y == x + 1
+        end
     end 
 
-    def y_coordinates_same?(coordinates)
+    def numbers_consecutive?(ship, coordinates)
+        numbers = x_coordinates_numbers(ship, coordinates)
+        numbers.each_cons(2).all? do |x,y|
+            y == x + 1
+        end
+    end
+
+    def same_letter?(ship, coordinates)
         y_coordinates = []
 
         split_coordinates = coordinates.map do |coordinate|
@@ -50,10 +83,11 @@ class Board
         split_coordinates.each do |coordinates|
           y_coordinates << coordinates[0]
         end
+
          y_coordinates.uniq.size == 1
     end
 
-    def x_coordinates_same?(coordinates)
+    def same_number?(ship, coordinates)
         x_coordinates = []
 
         split_coordinates = coordinates.map do |coordinate|
@@ -66,54 +100,25 @@ class Board
         x_coordinates.uniq.size == 1
     end
 
-    def convert_to_ordinals(letter_coords)
-        ordinal = []
-        letter_coords.each do |coordinate|
-        ordinal << coordinate.ord
-        end
-        return ordinal
+    def consecutive?(ship, coordinates)
+        same_number?(ship, coordinates) && letters_consecutive?(ship,coordinates) ||
+        same_letter?(ship, coordinates) && numbers_consecutive?(ship,coordinates) 
+    end 
+
+    def not_diagonal?(ship, coordinates) 
+        consecutive?(ship, coordinates) && same_letter?(ship, coordinates) || same_number?(ship, coordinates)
     end
 
-    def consecutive?()
-        split_coords.each_cons(2).all? do |x,y|
-        y == x + 1
-        end
-        true
+    def valid_placement?(ship, coordinates)
+        consecutive?(ship, coordinates) &&
+        not_diagonal?(ship, coordinates) &&
+        same_length?(ship, coordinates) 
     end
-
-    # def diagonal?(coordinates)
-    #     @y_coordinates_same? == true || @x_coordinates_same? == true
-    # end
-
-    def overlap?(ship, coordinates)
-        coordinates.all? do |cell|
-        @create_cells[cell].empty?
-        end
-    end
-
-    # def valid_placement?(ship, coordinates)
-    #     @consecutive? == true &&
-    #     @diagonal == false &&
-    #     @same_length == true &&
-    #     @overlap == false &&
-    #end
 
     def place(ship, coordinates)
         coordinates.each do |cell|
         @cells[cell].place_ship(ship)
         end
-    end
-
-    def split_horiz_coordinates
-        board_number_coords = @cells.keys
-        split_coords = split_coordinates(board_number_coords)
-        board_number_coords = store_number_coords(split_coords).uniq
-    end
-
-    def split_vert_coordinates
-        board_letter_coords = @cells.keys
-        split_coords = split_coordinates(board_letter_coords)
-        board_letter_coords = store_letter_coords(split_coords).uniq
     end
 
     def render_horizontal
@@ -148,14 +153,8 @@ class Board
     end
 end 
 
-    board = Board.new
-    p board.create_cells
-    p board.ordinals_array
-    p board.numbers_array
+board = Board.new
 
-    # cruiser = Ship.new("Cruiser", 3)
-    # submarine = Ship.new("Submarine", 2)
+board.create_cells
 
-    # p board.y_coordinates_same?(["A1", "A2", "A4"])
-    # p board.x_coordinates_same?(["A1", "A2", "A4"])
-    # p board.diagonal?(["A1", "A2", "A4"])
+p board.same_letter?(@cruiser, ["A2", "B3", "C4"])
--- test/board_test.rb
+++ test/board_test.rb
@@ -3,189 +3,115 @@ require 'minitest/pride'
 require './lib/board'
 
 class BoardTest < Minitest::Test
-  def test_board_exists
-    board = Board.new
+  def setup
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end 
 
-    assert_instance_of Board, board
+  def test_it_exists
+    assert_instance_of Board, @board
   end
 
   def test_that_cells_attribute_and_is_a_hash
-
-    board = Board.new
-
-    assert_equal Hash, board.cells.class
-    #assert_equal 16, board.cells.keys.length -- cannot test till as actual = 0
+    assert_equal Hash, @board.cells.class
   end
 
-    board = Board.new
+  def test_it_can_create_16_cells_with_key_value_pair
+    @board.create_cells
 
-    assert_equal Hash, board.cells.class
-    #assert_equal 16, board.cells.length -- cannot test till as actual = 0
-  end
-
-  def test_it_has_16_cells_with_key_value_pair
-    board = Board.new
-
-    assert_equal 16, board.create_cells.keys.length
-    assert_equal 16, board.create_cells.values.length 
+    assert_equal 16, @board.cells.length
+    assert_equal 16, @board.cells.keys.length
+    assert_equal 16, @board.cells.values.length
   end 
  
-  # def test_key_value_points_to_cell_objects #do i test for this? how? 
-  #   board = Board.new
-
-  #   assert_equal expected, actual 
+  # BELOW - do I test for this? how? 
+  # def test_key_value_points_to_cell_objects
+  #   expected = <Cell:0xXXXXXX @coordinate="A1", @ship=nil, @fire_upon=false>
+  #   assert_equal expected, @board.create_cells["A1"]
   # end 
 
-
-
-
   def test_coordinates_are_valid
-    board = Board.new
-
-    assert board.valid_coordinate?("A1") 
-    assert board.valid_coordinate?("D4")
+    assert @board.valid_coordinate?("A1") 
+    assert @board.valid_coordinate?("D4")
+    refute @board.valid_coordinate?("A5")
+    refute @board.valid_coordinate?("E1")
+    refute @board.valid_coordinate?("F2")
   end
 
   def test_coordinates_are_same_length
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    refute board.valid_placement?(cruiser, ["A1", "A2"])
-    refute board.valid_placement?(submarine, ["A2", "A3", "A4"])
-    assert board.valid_placement?(cruiser, ["A2", "A3", "A4"])
-    assert board.valid_placement?(submarine, ["A1", "A2"])
+    refute @board.same_length?(@submarine, ["A2", "A3", "A4"])
+    refute @board.same_length?(@cruiser, ["A1", "A2"])
+    assert @board.same_length?(@submarine, ["A1", "A2"])
+    assert @board.same_length?(@cruiser, ["A1", "A2", "A4"])
   end
+
+  def test_it_can_split_coordinates_into_a_flattened_array
+    expected = ["A", "2", "B", "3", "C", "4"]
+    actual = @board.split_coordinates(@submarine, ["A2", "B3", "C4"])
+
+    assert_equal expected, actual 
+  end 
+
+  def test_it_can_convert_letter_coordinates_into_ordinal_values 
+    expected = [65, 66, 68]
+    actual = @board.y_coordinates_ordinal_values(@cruiser, ["A2", "B3", "D4"])
+
+    assert_equal expected, actual 
+  end 
+
+  def test_it_can_convert_number_coordinates_into_integers
+    expected = [2, 3, 4]
+    actual = @board.x_coordinates_numbers(@cruiser, ["A2", "B3", "C4"])
+
+    assert_equal expected, actual
+  end 
+
+  def test_it_can_return_if_letters_are_consecutive
+
+  end
+
+  def test_it_can_return_if_numbers_are_consecutive
+  end 
+
+  def test_it_can_return_if_letter_coordinates_are_the_same
+    assert @board.same_letter?(@cruiser, ["A2", "A3", "A4"])
+    refute @board.same_letter?(@cruiser, ["A2", "B3", "A4"])
+  end 
+
+  def test_it_can_return_if_number_coordinates_are_the_same
+    assert @board.same_number?(@cruiser, ["A3", "A3", "A3"])
+    refute @board.same_number?(@cruiser, ["A2", "B3", "A4"])
+  end 
 
   def test_coordinates_are_consecutive
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    refute board.valid_placement?(cruiser, ["A1", "A2", "A4")
-    refute board.valid_placement?(submarine, ["A1", "C1"])
-    refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    refute board.valid_placement?(submarine, ["C1", "B1"])
-    #make assert tests
-  end
-
-<<<<<<< HEAD
-  def test_coordinates_are_not_diagonal 
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-  
-    refute board.valid_placement?(cruiser, ["A1", "B2", "C3")
-    refute board.valid_placement?(submarine, ["C2", "D3"]
-    #make assert tests
+    refute @board.consecutive?(@cruiser, ["A1", "A2", "A4"])
+    refute @board.consecutive?(@submarine, ["A1", "C1"])
+    refute @board.consecutive?(@cruiser, ["A3", "A2", "A1"])
+    refute @board.consecutive?(@submarine, ["C1", "B1"])
+    assert @board.consecutive?(@cruiser, ["B1", "B2" "B3"])
   end 
 
-  def test_placements_are_valid 
-=======
-  # def test_coordinates_valid 
-  #   board = Board.new
-
-  def test_coordinates_not_valid 
-    board = Board.new
-
-  
-  #   refute board.valid_coordinate?("A5")
-  #   refute board.valid_coordinate?("E1")
-  #   refute board.valid_coordinate?("A22")
-  # end
-
-
-  # def test_if_coordinates_and_ship_are_same_length
-  #   skip
-  #   board = Board.new
-  #   cruiser = Ship.new("Cruiser", 3)
-  #   submarine = Ship.new("Submarine", 2)
-
-    
-
-  #   refute board.same_length?(cruiser, ["A1", "A2"])
-  #   assert board.same_length?(cruiser, ["A1", "A2", "A3"])
-  #   refute board.same_length?(submarine, ["A1", "A2", "A3"])
-  #   assert board.same_length?(submarine, ["A1", "A2"])
-  # end 
-
-  # def test_if_coordinates_are_consecutive
-
-
-  #   refute board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-  #   refute board.valid_placement?(submarine, ["A1", "C1"])
-  #   refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-  # end
-
-  # def test_if_coordinates_are_diagonal
-  # end 
-
-  def test_if_coordinates_and_ship_are_same_length
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    refute board.same_length?(cruiser, ["A1", "A2"])
-    assert board.same_length?(cruiser, ["A1", "A2", "A3"])
-    refute board.same_length?(submarine, ["A1", "A2", "A3"])
-    assert board.same_length?(submarine, ["A1", "A2"])
+  def test_coordinates_are_not_diagonal
+    refute @board.not_diagonal?(@cruiser, ["A1", "B2", "C3"])
+    refute @board.not_diagonal?(@submarine, ["C2", "D3"])
+    assert @board.not_diagonal?(@cruiser, ["C1", "C2", "C3"])
+    assert @board.not_diagonal?(@submarine, ["D1", "D2"])
   end 
 
-  def test_if_coordinates_are_consecutive
-    skip
->>>>>>> 4c703417d49a7e53e7f6c4a307f72c5553b8fa43
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-<<<<<<< HEAD
-    assert board.valid_placement?(cruiser, ["B1", "C1", "D1")
-    assert board.valid_placement?(submarine, ["A1", "A2"]
-    refute board.valid_placement?(cruiser, ["A1", "B2", "C3")
-    refute board.valid_placement?(submarine, ["C2", "D3"]
+  def test_placements_are_valid
+    assert @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
+    refute @board.valid_placement?(@cruiser, ["A1", "C3"])
+    refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
   end
 
-  def test_if_ships_can_be_placed_on_cells
-  end
+#   def test_if_ships_can_be_placed_on_cells
+#   end
 
-  def test_if_cells_contain_the_same_ship
-  end
+#   def test_if_cells_contain_the_same_ship
+#   end
 
-  def test_if_ships_overlap
-  end 
-
-  def 
-end
-=======
-    refute board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    refute board.valid_placement?(submarine, ["A1", "C1"])
-    refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    refute board.valid_placement?(cruiser, ["C1", "B1"])
-  end
-
-  def test_if_coordinates_are_diagonal
-    skip
-  end 
-
-
-  # def test_if_placements_are_valid 
-  #   skip
-  #   board = Board.new
-  #   cruiser = Ship.new("Cruiser", 3)
-  #   submarine = Ship.new("Submarine", 2)
-
-  #   refute board.valid_placement?(cruiser, ["A1", "A2"])
-  #   refute board.valid_placement?(submarine, ["A2", "A3", "A4"])
-  # end
- 
-
-  # def test_placements_are_not_valid #this test is the refute assertion in the test above 
-  # end
-
-  def test_placements_are_not_valid
-    skip #this test is the refute assertion in the test above 
-  end
-
-
-end
->>>>>>> 4c703417d49a7e53e7f6c4a307f72c5553b8fa43
+#   def test_if_ships_overlap
+#   end 
+end 
