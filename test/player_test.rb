d1c67e9bb65b47f9cdeb41a8372cf943d1da695b
Martha Troubh <indexer@icloud.com>
2019-02-06 13:23:16 -0700

    Add Player test

---
+++ test/player_test.rb
@@ -0,0 +1,12 @@
+require './lib/player'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class PlayerTest < Minitest::Test
+
+  def test_it_exists
+    player = Player.new
+    assert_instance_of Player, player
+  end
+
+end
